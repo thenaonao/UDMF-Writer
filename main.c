fbc8f839a30b6762604f7fe4620b84ff75e20924
NTK <jean-cedric.petit@u-psud.fr>
2023-01-15 18:06:35 +0100

    A bit of math. Prepared for static memory allocation of the arrays.

--- main.c
+++ main.c
@@ -3,7 +3,7 @@
 #include <string.h>
 #include <time.h>
 
-#define C_SIZE 1024
+#define C_SIZE 256
 
 struct vertex{
     int id;
@@ -13,37 +13,41 @@ struct vertex{
 };
 
 struct linedef{
-    int id;
-    int id_V1;
+    int id; //Linedef ID
+    int id_V1;//Vertex ID reference
     int id_V2;
-    int blocking;
-    int special;
-    //dont draw flag needed
-    int id_S1;//Sidedef front
-    int id_S2;
+    int blocking; //Should be 0 Unless its on the exterior of the square
+    int special; //Exterior should have =9, else its 0.
+    //Do not forget "dont draw" flag
+    int id_S1;//Sidedef front reference
+    int id_S2; //-1 default
 };
 
 struct sidedef{
-    int sector;
+    int sector; //sector ID reference
     char Textmiddle[9]; //Textures name are 8 char length, but +1 for the str end.
 };
 
 struct sector{
-    int floor;
-    int ceil;
+    int floor;//Floor height
+    int ceil;//Ceil height Both should be 0, because we use the vertices to draw the terrain.
     
     char* textfloor;
-    char* textceil;
+    char* textceil; //texturefloor = "FLOOR0_1"; textureceiling = "CEIL1_1";
     
-    int lightlevel;
-    int id;
+    int lightlevel; //Can be interesing if we want to cast fixed shadow from terrain... (For the future heh)
+    int id; //Sector ID
 };
 
 
 
 struct vertex vertices[C_SIZE*C_SIZE];
+struct linedef linedefs[(C_SIZE-1)*(C_SIZE+(2*C_SIZE-1))]; //Ouch thats a lot!
+struct sidedef sidedefs[(C_SIZE-1)*(C_SIZE+(2*C_SIZE-1))]; //should be the same as linedefs
+struct sector sectors[2*(C_SIZE-1)*(C_SIZE-1)];
 
 int main(int argc, char *argv[]){
+    float h=8192.0/C_SIZE;
     srand(time(NULL));
     
     int counter=0;
@@ -51,8 +55,8 @@ int main(int argc, char *argv[]){
     for(float y=0;y<C_SIZE;y++){
         for(float x=0;x<C_SIZE;x++){
             vertices[(int)y*C_SIZE+(int)x].id=counter;
-            vertices[(int)y*C_SIZE+(int)x].x=x;
-            vertices[(int)y*C_SIZE+(int)x].y=y;
+            vertices[(int)y*C_SIZE+(int)x].x=h*x;
+            vertices[(int)y*C_SIZE+(int)x].y=h*y;
             vertices[(int)y*C_SIZE+(int)x].z=0.0; //(float) (rand()%64);
             counter++;
         }
