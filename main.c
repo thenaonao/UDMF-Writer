2d149979799eb73f5b0d214dcb366d1279e93e98
NTK <jean-cedric.petit@u-psud.fr>
2023-01-15 20:30:20 +0100

    Added Linedef to the map lump. This will not work yet.

--- main.c
+++ main.c
@@ -3,29 +3,32 @@
 #include <string.h>
 #include <time.h>
 
-#define C_SIZE 256
+#define C_SIZE 32
+#define C_MAP_LENGTH 8192.0
 
 struct vertex{
-    int id;
+    unsigned int id;
     float x;
     float y;
     float z;
 };
 
 struct linedef{
-    int id; //Linedef ID
-    int id_V1;//Vertex ID reference
-    int id_V2;
-    int blocking; //Should be 0 Unless its on the exterior of the square
+    unsigned int id; //Linedef ID
+    unsigned int id_V1;//Vertex ID reference
+    unsigned int id_V2;
+    unsigned int blocking; //Should be 0 Unless its on the exterior of the square
+    unsigned int twosided; //Should be 1 if its sandwiched between 2 sectors
     int special; //Exterior should have =9, else its 0.
     //Do not forget "dont draw" flag
-    int id_S1;//Sidedef front reference
-    int id_S2; //-1 default
+    unsigned int id_S1;//Sidedef front reference
+    unsigned int id_S2; //-1 default
 };
 
 struct sidedef{
-    int sector; //sector ID reference
+    unsigned int sector; //sector ID reference
     char Textmiddle[9]; //Textures name are 8 char length, but +1 for the str end.
+                        // STARTAN2
 };
 
 struct sector{
@@ -36,7 +39,7 @@ struct sector{
     char* textceil; //texturefloor = "FLOOR0_1"; textureceiling = "CEIL1_1";
     
     int lightlevel; //Can be interesing if we want to cast fixed shadow from terrain... (For the future heh)
-    int id; //Sector ID
+    unsigned int id; //Sector ID
 };
 
 
@@ -47,22 +50,187 @@ struct sidedef sidedefs[(C_SIZE-1)*(C_SIZE+(2*C_SIZE-1))]; //should be the same 
 struct sector sectors[2*(C_SIZE-1)*(C_SIZE-1)];
 
 int main(int argc, char *argv[]){
-    float h=8192.0/C_SIZE;
+    float h=C_MAP_LENGTH/C_SIZE;
     srand(time(NULL));
     
     int counter=0;
-    printf("Creating the vertices..........");
+
+    printf("Creating the Vertices");
     for(float y=0;y<C_SIZE;y++){
         for(float x=0;x<C_SIZE;x++){
+            //Vertices
             vertices[(int)y*C_SIZE+(int)x].id=counter;
             vertices[(int)y*C_SIZE+(int)x].x=h*x;
             vertices[(int)y*C_SIZE+(int)x].y=h*y;
             vertices[(int)y*C_SIZE+(int)x].z=0.0; //(float) (rand()%64);
+
+            if( ((int)y*C_SIZE+(int)x)%(C_SIZE*C_SIZE/10)==0 &&((int)y*C_SIZE+(int)x)!=0){
+                printf(".");
+            }
             counter++;
         }
     }
     printf("Finished!\n");
 
+    counter=0;
+    printf("Creating the Linedefs & Sidedefs");
+    for(float y=0;y<C_SIZE;y++){
+        for(float x=0;x<C_SIZE;x++){
+            if(x==C_SIZE-1 && y==C_SIZE-1){
+                continue;
+            }else if(x==C_SIZE-1){
+                linedefs[counter].id=counter;
+                linedefs[counter].id_V1=(int)(x+y*C_SIZE);
+                linedefs[counter].id_V2=(int)(x+y*C_SIZE)+C_SIZE; //Should be the vertex on the same column but +1 line higher.
+                linedefs[counter].blocking=1;
+                linedefs[counter].twosided=0;
+                linedefs[counter].special=9;
+                linedefs[counter].id_S1=counter;
+                linedefs[counter].id_S2=-1;
+                counter++;
+            }else if (y==C_SIZE-1){
+                linedefs[counter].id=counter;
+                linedefs[counter].id_V1=(int)(x+y*C_SIZE);
+                linedefs[counter].id_V2=(int)(x+y*C_SIZE)+1; //Should be the vertex on the right.
+                linedefs[counter].blocking=1;
+                linedefs[counter].twosided=0;
+                linedefs[counter].special=9;
+                linedefs[counter].id_S1=counter;
+                linedefs[counter].id_S2=-1;
+                counter++;
+            }else if(x==0 && y==0){
+                //L1
+                linedefs[counter].id=counter;
+                linedefs[counter].id_V1=(int)(x+y*C_SIZE);
+                linedefs[counter].id_V2=(int)(x+y*C_SIZE)+1; //Should be the vertex on the right.
+                linedefs[counter].blocking=1;
+                linedefs[counter].twosided=0;
+                linedefs[counter].special=9;
+                linedefs[counter].id_S1=counter;
+                linedefs[counter].id_S2=-1;
+                counter++;
+                //L2
+                linedefs[counter].id=counter;
+                linedefs[counter].id_V1=(int)(x+y*C_SIZE);
+                linedefs[counter].id_V2=(int)(x+y*C_SIZE)+C_SIZE; //Should be the vertex on the same column but +1 line higher.
+                linedefs[counter].blocking=1;
+                linedefs[counter].twosided=0;
+                linedefs[counter].special=9;
+                linedefs[counter].id_S1=counter;
+                linedefs[counter].id_S2=-1;
+                counter++;
+                //L3
+                linedefs[counter].id=counter;
+                linedefs[counter].id_V1=(int)(x+y*C_SIZE)+1;  //Should be the vertex on the right.
+                linedefs[counter].id_V2=(int)(x+y*C_SIZE)+C_SIZE; //Should be the vertex on the same column but +1 line higher.
+                linedefs[counter].blocking=0;
+                linedefs[counter].twosided=1;
+                linedefs[counter].special=0;
+                linedefs[counter].id_S1=counter;
+                linedefs[counter].id_S2=-1;
+                counter++;                
+            }else if(x==0){
+                //L1
+                linedefs[counter].id=counter;
+                linedefs[counter].id_V1=(int)(x+y*C_SIZE);
+                linedefs[counter].id_V2=(int)(x+y*C_SIZE)+1; //Should be the vertex on the right.
+                linedefs[counter].blocking=0;
+                linedefs[counter].twosided=1;
+                linedefs[counter].special=0;
+                linedefs[counter].id_S1=counter;
+                linedefs[counter].id_S2=-1;
+                counter++;
+                //L2
+                linedefs[counter].id=counter;
+                linedefs[counter].id_V1=(int)(x+y*C_SIZE);
+                linedefs[counter].id_V2=(int)(x+y*C_SIZE)+C_SIZE; //Should be the vertex on the same column but +1 line higher.
+                linedefs[counter].blocking=1;
+                linedefs[counter].twosided=0;
+                linedefs[counter].special=9;
+                linedefs[counter].id_S1=counter;
+                linedefs[counter].id_S2=-1;
+                counter++;
+                //L3
+                linedefs[counter].id=counter;
+                linedefs[counter].id_V1=(int)(x+y*C_SIZE)+1;  //Should be the vertex on the right.
+                linedefs[counter].id_V2=(int)(x+y*C_SIZE)+C_SIZE; //Should be the vertex on the same column but +1 line higher.
+                linedefs[counter].blocking=0;
+                linedefs[counter].twosided=1;
+                linedefs[counter].special=0;
+                linedefs[counter].id_S1=counter;
+                linedefs[counter].id_S2=-1;
+                counter++;
+            }else if(y==0){
+                //L1
+                linedefs[counter].id=counter;
+                linedefs[counter].id_V1=(int)(x+y*C_SIZE);
+                linedefs[counter].id_V2=(int)(x+y*C_SIZE)+1; //Should be the vertex on the right.
+                linedefs[counter].blocking=1;
+                linedefs[counter].twosided=0;
+                linedefs[counter].special=9;
+                linedefs[counter].id_S1=counter;
+                linedefs[counter].id_S2=-1;
+                counter++;
+                //L2
+                linedefs[counter].id=counter;
+                linedefs[counter].id_V1=(int)(x+y*C_SIZE);
+                linedefs[counter].id_V2=(int)(x+y*C_SIZE)+C_SIZE; //Should be the vertex on the same column but +1 line higher.
+                linedefs[counter].blocking=0;
+                linedefs[counter].twosided=1;
+                linedefs[counter].special=0;
+                linedefs[counter].id_S1=counter;
+                linedefs[counter].id_S2=-1;
+                counter++;
+                //L3
+                linedefs[counter].id=counter;
+                linedefs[counter].id_V1=(int)(x+y*C_SIZE)+1;  //Should be the vertex on the right.
+                linedefs[counter].id_V2=(int)(x+y*C_SIZE)+C_SIZE; //Should be the vertex on the same column but +1 line higher.
+                linedefs[counter].blocking=0;
+                linedefs[counter].twosided=1;
+                linedefs[counter].special=0;
+                linedefs[counter].id_S1=counter;
+                linedefs[counter].id_S2=-1;
+                counter++;
+            }else{
+                //L1
+                linedefs[counter].id=counter;
+                linedefs[counter].id_V1=(int)(x+y*C_SIZE);
+                linedefs[counter].id_V2=(int)(x+y*C_SIZE)+1; //Should be the vertex on the right.
+                linedefs[counter].blocking=0;
+                linedefs[counter].twosided=1;
+                linedefs[counter].special=0;
+                linedefs[counter].id_S1=counter;
+                linedefs[counter].id_S2=-1;
+                counter++;
+                //L2
+                linedefs[counter].id=counter;
+                linedefs[counter].id_V1=(int)(x+y*C_SIZE);
+                linedefs[counter].id_V2=(int)(x+y*C_SIZE)+C_SIZE; //Should be the vertex on the same column but +1 line higher.
+                linedefs[counter].blocking=0;
+                linedefs[counter].twosided=1;
+                linedefs[counter].special=0;
+                linedefs[counter].id_S1=counter;
+                linedefs[counter].id_S2=-1;
+                counter++;
+                //L3
+                linedefs[counter].id=counter;
+                linedefs[counter].id_V1=(int)(x+y*C_SIZE)+1;  //Should be the vertex on the right.
+                linedefs[counter].id_V2=(int)(x+y*C_SIZE)+C_SIZE; //Should be the vertex on the same column but +1 line higher.
+                linedefs[counter].blocking=0;
+                linedefs[counter].twosided=1;
+                linedefs[counter].special=0;
+                linedefs[counter].id_S1=counter;
+                linedefs[counter].id_S2=-1;
+                counter++;
+            }
+            if( ((int)y*C_SIZE+(int)x)%(C_SIZE*C_SIZE/10)==0 &&((int)y*C_SIZE+(int)x)!=0){
+                printf(".");
+            }
+        }   
+    }
+    printf("Finished!\n");
+
+    printf("Creating TEXTMAP Lump.\n");
     FILE *fptr;
     fptr=fopen("TEXTMAP","w+");
     
@@ -70,11 +238,6 @@ int main(int argc, char *argv[]){
     printf("Writing Header..........");
     fprintf(fptr,"%s","namespace = \"zdoom\";\n");
     printf("Finished!\n");
-
-    //Writes each linedefs
-
-    //Writes each sidedefs
-
 
     //Writes each vertex in the file
     printf("Writing Vertices");
@@ -86,6 +249,18 @@ int main(int argc, char *argv[]){
     }
     printf("Finished!\n");
 
+    //Writes each linedefs & sidedefs... They should be written in the same loop.(Because we have no backsided sidedefs).
+    printf("Writing Linedefs & Sidedefs");
+    for(int i=0;i<(C_SIZE-1)*(C_SIZE+(2*C_SIZE-1));i++){
+        if(i!=0 && linedefs[i].id==0){
+            continue;
+        }
+        fprintf(fptr,"linedef // %d\n{\nv1 = %d;\nv2 = %d;\nsidefront = %d;\nsideback = %d;\n%s = true;\n}\n\n",linedefs[i].id,linedefs[i].id_V1,linedefs[i].id_V2,linedefs[i].id_S1,linedefs[i].id_S2,(linedefs[i].blocking==1)?"blocking":"twosided");
+        if(i%((C_SIZE-1)*(C_SIZE+(2*C_SIZE-1))/10)==0 &&i!=0){
+            printf(".");
+        }
+    }
+    printf("Finished!\n");
     //Writes each sectors...
 
     //Things here...
